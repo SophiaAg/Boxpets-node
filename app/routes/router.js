be6455c7db674c051b621429f8a63f228297180c
Sophia Araújo <116041632+SophiaAg@users.noreply.github.com>
2024-04-09 02:27:20 +0000

    ss

---
+++ app/routes/router.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var router = express.Router();
+
+router.get("/", function (req, res) {
+    res.render("pages/LandingPage", {pagina:"LandingPage"});
+});
+
+router.get("/pages/login", function (req, res) {
+    res.render("pages/login");
+});
+
+router.get("/pages", function (req, res) {
+    res.render("pages/login", {pagina:"sss"});
+});
+
+router.get("/", function (req, res) {
+    res.render("pages/LandingPage", {pagina:"LandingPage"});
+});
+
+router.get("/", function (req, res) {
+    res.render("pages/LandingPage", {pagina:"LandingPage"});
+});
+
+router.get("/", function (req, res) {
+    res.render("pages/LandingPage", {pagina:"LandingPage"});
+});
+
+router.get("/", function (req, res) {
+    res.render("pages/LandingPage", {pagina:"LandingPage"});
+});
+
+
+module.exports = router;
