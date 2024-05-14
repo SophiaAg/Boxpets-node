cf3165c12571f66105d31812b7aa26820eb873e4
SophiaAg <sophiafroez7@gmail.com>
2024-05-14 00:07:41 -0300

    00h07

--- app/public/js/landingPage.js
+++ app/public/js/landingPage.js
@@ -1,23 +1,76 @@
-let navbar = document.querySelector('.header .navbar');
-
-document.querySelector('#menu-btn').onclick = () =>{
-    navbar.classList.toggle('active');
+let nextDom = document.getElementById('next');
+let prevDom = document.getElementById('prev');
+ 
+let carouselDom = document.querySelector('.carousel');
+let SliderDom = carouselDom.querySelector('.carousel .list');
+let thumbnailBorderDom = document.querySelector('.carousel .thumbnail');
+ 
+let timeDom = document.querySelector('.carousel .time');
+ 
+let timeRunning = 3000;
+let timeAutoNext = 7000;
+ 
+nextDom.onclick = function(){
+    showSlider('next');    
 }
-
-window.onscroll = () =>{
-    navbar.classList.remove('active');
-
-    if(window.screenY > 0){
-        document.querySelector('.header').classList.add('active');
+ 
+prevDom.onclick = function(){
+    showSlider('prev');    
+}
+let runTimeOut;
+let runNextAuto = setTimeout(() => {
+    next.click();
+}, timeAutoNext)
+function showSlider(type){
+    let  SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
+    let thumbnailItemsDom = document.querySelectorAll('.carousel .thumbnail .item');
+   
+    if(type === 'next'){
+        SliderDom.appendChild(SliderItemsDom[0]);
+        thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+        carouselDom.classList.add('next');
     }else{
-        document.querySelector('.header').classList.remove('active');
+        SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
+        thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
+        carouselDom.classList.add('prev');
     }
+    clearTimeout(runTimeOut);
+    runTimeOut = setTimeout(() => {
+        carouselDom.classList.remove('next');
+        carouselDom.classList.remove('prev');
+    }, timeRunning);
+ 
+    clearTimeout(runNextAuto);
+    runNextAuto = setTimeout(() => {
+        next.click();
+    }, timeAutoNext)
 }
-
-window.onload= () =>{
-    if(window.screenY > 0){
-        document.querySelector('.header').classList.add('active');
+ 
+//navbar
+ 
+const toggleBtn = document.querySelector('.toggle_btn')
+const toggleBtnIcon = document.querySelector('.toggle_btn i')
+const dropDownMenu = document.querySelector('.dropdown_menu')
+ 
+toggleBtn.onclick = function () {
+    dropDownMenu.classList.toggle('open')
+    const isOpen = dropDownMenu.classList.contains('open')
+ 
+    toggleBtnIcon.classList = isOpen
+    ? 'fa-solid fa-xmark'
+    : 'fa-solid fa-bars'
+}
+ 
+//modo noturno
+ 
+let darkmode = document.querySelector('#darkmode');
+ 
+darkmode.onclick = () => {
+    if(darkmode.classList.contains('fa-moon')){
+        darkmode.classList.replace('fa-moon', 'fa-sun');
+        document.body.classList.add('color');
     }else{
-        document.querySelector('.header').classList.remove('active');
+        darkmode.classList.replace('fa-sun', 'fa-moon');
+        document.body.classList.remove('color')
     }
 }
