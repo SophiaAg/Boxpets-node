445c26870b7984c7af1bac1c2c9c5cdbf21217f0
SophiaAg <sophiafroez7@gmail.com>
2024-05-31 01:45:39 -0300

    1h45

--- app/routes/router.js
+++ app/routes/router.js
@@ -5,6 +5,10 @@ router.get("/", function (req, res) {
     res.render("pages/template-lp", {pagina:"LandingPage", page:"../partial/landing-page/lp-inicial"});
 });
 
+router.get("/souempresa", function (req, res) {
+    res.render("pages/template-lp", {pagina:"LandingPage", page:"../partial/landing-page/souempresa"});
+});
+
 router.get("/login", function (req, res) {
     res.render("pages/login");
 });
