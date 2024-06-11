2d92f28d6de9e103e3f34350d8e63b715735ffe2
th0mzzz <contatothomazvmendes@gmail.com>
2024-06-10 22:43:43 -0300

    att.

--- app/routes/router.js
+++ app/routes/router.js
@@ -3,24 +3,28 @@ const usuariosController = require("../controllers/usersController");
 var router = express.Router();
 
 router.get("/", function (req, res) {
-    res.render("pages/template-lp", {pagina:"LandingPage", page:"../partial/landing-page/lp-inicial"});
+    res.render("pages/template-lp", { pagina: "LandingPage", page: "../partial/landing-page/lp-inicial" });
 });
 
 router.get("/souempresa", function (req, res) {
-    res.render("pages/template-lp", {pagina:"LandingPage", page:"../partial/landing-page/souempresa"});
+    res.render("pages/template-lp", { pagina: "LandingPage", page: "../partial/landing-page/souempresa" });
 });
 
-router.get("/login", function (req, res) {
-    res.render("pages/login");
+router.get("/entrar", function (req, res) {
+    res.render("pages/template-login", { form: "../partial/login/entrar", isCadastrar: false });
 });
+router.get("/cadastrar", function (req, res) {
+    res.render("pages/template-login", { form: "../partial/login/cadastrar", isCadastrar: true });
+});
+
 router.get("/template-hm", function (req, res) {
-    res.render("pages/template-hm", {pagina:"LandingPage", page:"../pages/template-hm"});
+    res.render("pages/template-hm", { pagina: "LandingPage", page: "../pages/template-hm" });
 });
 
 
 // Cadastro de usuários
-router.post("/cadastrarUsuario", function(req,res){
-    usuariosController.cadastrar(req,res)
+router.post("/cadastrarUsuario", function (req, res) {
+    usuariosController.cadastrar(req, res)
 })
 
 
