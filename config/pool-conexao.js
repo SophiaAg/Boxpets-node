36b23177422eaca2098af73b55de0aa3db61a974
SophiaAg <sophiafroez7@gmail.com>
2024-06-27 21:22:15 -0300

    vaipff

--- config/pool-conexao.js
+++ config/pool-conexao.js
@@ -2,11 +2,11 @@ const mysql = require('mysql2')
 const dotenv = require("dotenv");
 dotenv.config();
 const pool = mysql.createPool({
-    host: process.env.HOST, 
-    user: process.env.USER, 
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-    port: process.env.PORT,
+    host: process.env.DB_HOST, 
+    user: process.env.DB_USER, 
+    password: process.env.DB_PASWORDITB,
+    database: process.env.DB_NAME,
+    port: process.env.APP_PORT,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
