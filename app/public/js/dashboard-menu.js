9e5c5263db2b5132eae62885c9d670eb4a67e38b
SophiaAg <sophiafroez7@gmail.com>
2024-10-25 01:03:56 -0300

    01h03

--- app/public/js/dashboard-menu.js
+++ app/public/js/dashboard-menu.js
@@ -3,26 +3,25 @@ const menuBtn = document.querySelector("#menu-btn");
 const closeBtn = document.querySelector("#close-btn");
 const sidebarItems = document.querySelectorAll('.container .menulateral .slidebar .item');
 
-menuBtn.addEventListener('click', () =>{ 
+// Abrir e fechar o menu lateral
+menuBtn.addEventListener('click', () => {
     sideMenu.style.display = 'block';
-    })
+});
 
-    closeBtn.addEventListener('click', () => {
-        sideMenu.style.display = 'none';
-    })
+closeBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'none';
+});
 
-    //active
-
-    let activeItem = sidebarItems[0];
+// Marcar item como ativo
+let activeItem = null; // Inicializa como null para garantir que não haja nenhum item ativo inicialmente
 
 sidebarItems.forEach(element => {
     element.addEventListener('click', () => {
         if (activeItem) {
-            activeItem.removeAttribute('id');
+            activeItem.removeAttribute('id'); // Remove o 'id' do item anteriormente ativo
         }
 
-        element.setAttribute('id', 'active');
-        activeItem = element;
-
+        element.setAttribute('id', 'active'); // Define o novo 'id' ativo
+        activeItem = element; // Atualiza o item ativo
     });
 });
